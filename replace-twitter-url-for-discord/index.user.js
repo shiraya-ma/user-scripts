dc6ac4c444e1fc1b55095bdf531785de3096ce53
shirayama-mai <138377007+shirayama-mai@users.noreply.github.com>
2023-12-21 19:49:33 +0900

    first commit

---
+++ replace-twitter-url-for-discord/index.user.js
@@ -0,0 +1,15 @@
+"use strict";
+// ==UserScript==
+// @name            Discord用URLをコピー
+// @namespace       https://twitter.com/mai_shirayama
+// @version         v0.0.0
+// @description     Copy the URL with the domain replaced by "vxtwitter.com" to the clipboard.
+// @author          @mai_shirayama
+// @match           https://*/*
+// @run-at          document-idle
+// @noframes
+// ==/UserScript==
+(() => {
+    'use stricts';
+    console.log('hello world!');
+})();
---
+++ replace-twitter-url-for-discord/index.user.ts
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name            Discord用URLをコピー
+// @namespace       https://twitter.com/mai_shirayama
+// @version         v0.0.1
+// @description     Copy the URL with the domain replaced by "vxtwitter.com" to the clipboard.
+// @author          @mai_shirayama
+// @match           https://x.com/*
+// @match           https://twitter.com/*
+// @grant           GM_setClipboard
+// @grant           GM_registerMenuCommand
+// @run-at          document-idle
+// @noframes
+// ==/UserScript==
+
+(() => {
+    'use stricts';
+
+    console.log('hello world!');
+})();

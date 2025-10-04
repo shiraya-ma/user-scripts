f7a1cd5aedd746b5582d4236df964d19beb2a5d7
shiraya-ma <mai.shirayama@outlook.jp>
2025-10-04 21:23:05 +0900

    :tada: add script

---
+++ gh-add-emoji-prefix/index.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         Add Emoji Prefix on PR
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0-alpha
+// @description  try to take over the world!
+// @author       @shiraya-ma
+// @match        https://github.com/shiraya-ma/xmlrpc-json/pull/6
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(() => {
+  'use strict';
+
+  onOpenMergePullRequestModal((input) => {
+    const originalValue = input.value;
+
+    if (/^:\w+:\s/.test(originalValue)) {
+      return;
+    }
+
+    input.value = `:twisted_rightwards_arrows: ${originalValue}`;
+
+    console.log('updated merge message:', input.value);
+  });
+})();
+
+/**
+ * 
+ * @param {OnOpenCallback} callback 
+ */
+function onOpenMergePullRequestModal (callback) {
+  let shown = false;
+
+  const observer = new MutationObserver(() => {
+    /** @type { HTMLInputELement | null } */
+    const input = document.querySelector('div.merge-pr input');
+
+    if (!input) {
+      shown = false;
+      return;
+    }
+
+    if (!shown) {
+      shown = true;
+      callback(input);
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+};
+
+/**
+ * @typedef { (input: HTMLInputElement) => void } OnOpenCallback
+ */
